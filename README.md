6dd5ba9406b9309cace7b1fe2fdb482759168e32
Saleh Sayeem <16103179@iubat.edu>
2023-07-17 19:25:50 +0600

    Download pdf

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # NgHtmlToPdf
 
+npm install jspdf @types/jspdf
+npm install jspdf html2canvas
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.7.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-html-to-pdf';
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+  previewPDF() {
+    const doc = new jsPDF('p', 'mm', 'a4');
+    const element = this.elementRef.nativeElement.querySelector('#ss');
+    html2canvas(element).then((canvas) => {
+      console.log(canvas)
+      const imgData = canvas.toDataURL('image/png');
+      doc.addImage(imgData, 'PNG', 10, 10, doc.internal.pageSize.getWidth() - 20, 0);
+      doc.save('preview.pdf');
+    });
+  }
 }
